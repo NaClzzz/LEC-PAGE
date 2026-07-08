44b75d33776f22f09c24dbdc5572cefe005d7702
_NaCl_ <xuanshaohui163@163.com>
2026-07-08 16:20:44 +0800

    ver

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/LEC-PAGE/',
+  base: process.env.VERCEL ? '/' : '/LEC-PAGE/',
   plugins: [react(), tailwindcss()],
   assetsInclude: ['**/*.glb'],
 })
