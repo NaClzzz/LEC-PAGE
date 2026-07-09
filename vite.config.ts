d2aae3bc12a5a39e11ca36905246089600eb4bae
_NaCl_ <xuanshaohui163@163.com>
2026-07-09 14:50:57 +0800

    nogithubpage

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: process.env.VERCEL ? '/' : '/LEC-PAGE/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   assetsInclude: ['**/*.glb'],
 })
